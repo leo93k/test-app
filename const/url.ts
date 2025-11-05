7044be3bf2c0d8bab9b2a19c02915c45fb0b75a8
leo <leo@funble.kr>
2025-11-05 10:57:50 +0900

    refactor: const 폴더 구조를 카테고리별로 분리
    - const/delay.ts 추가: 타임아웃 및 대기 시간 상수들
    - const/url.ts 추가: URL 관련 상수들
    - const/index.ts에서 export 재구성
    - 로컬 상수 제거 및 중앙 상수로 통합: loginFlow.ts, friendRequestFlow.ts

---
+++ const/url.ts
@@ -0,0 +1,5 @@
+export const LOGIN_URL =
+    "https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/";
+
+export const NAVER_BLOG_SEARCH_URL =
+    "https://section.blog.naver.com/Search/Blog.naver";
